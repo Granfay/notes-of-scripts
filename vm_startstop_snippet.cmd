78789e84166a67acd89a39757238fc4fdb538905
Mutsushi Taoda <fay-tech@memoad.jp>
2016-01-29 11:28:08 +0900

    first commit

---
+++ vm_startstop_snippet.cmd
@@ -0,0 +1,4 @@
+set PATH=C:\Program Files\Oracle\VirtualBox\;%PATH%
+rem vboxmanage startvm "Target VM name" --type headless
+rem vboxmanage controlvm "Target VM name" acpipowerbutton
+rem vboxmanage showvminfo "Target VM name"
